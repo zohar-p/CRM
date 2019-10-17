95a993de93e5dab98cc0564161bfedb11033e7ff
zohar pfeffer <pfzohar@gmail.com>
2019-10-17 08:56:46 +0300

    improved sales by select layout

--- src/components/analytics/Charts.js
+++ src/components/analytics/Charts.js
@@ -14,29 +14,35 @@ function Charts() {
 
     return (
         <Grid container spacing={3}>
-              <Grid item xs={12} md={6}>
-            <Paper>
-                <Grid container justify="center" alignItems="center" spacing={1}>
-                    <Grid item>
-                    <Typography variant='h4' display="inline">Sales By</Typography>
+            <Grid item xs={12} md={4}>
+                <Paper>
+                    <Grid container justify="center" alignItems="center" spacing={1}>
+                        <Grid item xs={6}>
+                        <Typography variant='h4' align="center">Sales By</Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <SelectInput for="category" setValue={setCategory}></SelectInput>
+                        </Grid>
+                        <Grid item>
+                            <Button color="primary" variant="contained" onClick={() => setChart('pie')}>
+                                <Icon>pie_chart</Icon>
+                            </Button>
+                        </Grid>
+                        <Grid item>
+                            <Button color="primary" variant="contained" onClick={() => setChart('bars')}>
+                                <Icon>bar_chart</Icon>
+                            </Button>
+                        </Grid>
                     </Grid>
-                    <Grid item>
-                    <SelectInput for="category" setValue={setCategory}></SelectInput>
-                    </Grid>
-                    <Grid item>
-                        <Button color="primary" variant="contained" onClick={() => setChart('pie')}>
-                            <Icon>pie_chart</Icon>
-                        </Button>
-                    </Grid>
-                    <Grid item>
-                        <Button color="primary" variant="contained" onClick={() => setChart('bars')}>
-                            <Icon>bar_chart</Icon>
-                        </Button>
-                    </Grid>
-                </Grid>
-                {chart === 'bars' ?  <TopEmployeesChart data={formatDataSalesByGroup(category.toLowerCase())} /> : chart === 'pie' ? <SalesByCountryChart data={formatDataSalesByGroup(category.toLowerCase())} /> : null }
-            </Paper>
-        </Grid>
+                    {chart === 'bars' ?  <TopEmployeesChart data={formatDataSalesByGroup(category.toLowerCase())} /> : chart === 'pie' ? <SalesByCountryChart data={formatDataSalesByGroup(category.toLowerCase())} /> : null }
+                </Paper>
+            </Grid>
+            <Grid item xs={12} md={8}>
+                <Paper>
+                    <Typography variant="h4" display="inline" align="center">Sales By Dates</Typography>
+                    
+                </Paper>
+            </Grid>
         </Grid>
     )
 }
